8f7144b8bd11657c0ed9a1f733689447ef82a0d6
takasaki <sh0g0.tksk@gmail.com>
2021-08-26 22:01:34 +0900

    ユークリッドの互助法

---
+++ practice/euclidean.rb
@@ -0,0 +1,17 @@
+def gcd(x, y)
+  # x >= y となる様に値を交換
+  x = [x, y].max
+  y = [x, y].min
+
+  while y > 0
+    r = x % y
+    x = y
+    y = r
+  end
+
+  x
+end
+
+x = gets.to_i
+y = gets.to_i
+puts gcd(x, y)
