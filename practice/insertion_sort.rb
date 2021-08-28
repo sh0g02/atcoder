b1c5d121af8b0c73f84e4323edc757888752f33f
takasaki <sh0g0.tksk@gmail.com>
2021-08-28 10:05:44 +0900

    add comment

--- practice/insertion_sort.rb
+++ practice/insertion_sort.rb
@@ -1,12 +1,15 @@
 def insertion_sort(array)
   (1..array.size - 1).each do |i|
     p array
+    # 未ソートの部分の先頭から要素を取得して記録
     v = array[i]
     j = i - 1
     while j >= 0 && array[j] > v
+      # ソート済の部分において、vより大きい要素を後方へ1個移動
       array[j + 1] = array[j]
       j -= 1
     end
+    # 空いた部分にvを挿入
     array[j + 1] = v
   end
 end
