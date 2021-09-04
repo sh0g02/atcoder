16b2bd7b78b00bf48baaa775a68fc609fe2e46ad
tksk <takasaki@inagora.com>
2021-09-04 11:45:55 +0900

    add

--- practice/my_stack.rb
+++ practice/my_stack.rb
@@ -1,8 +1,13 @@
-# 一時的にデータを対比したいときに有利なデータ構造
+# frozen_string_literal: true
+
+# Last in First out
 class MyStack
+  # getter, setter
+  attr_accessor :stack
+
   def initialize
     @top = 0
-    @stack = []
+    @stack = Array.new(1000)
   end
 
   def empty?
@@ -16,6 +21,7 @@ class MyStack
   # 計算量はO(1)
   def push(x)
     raise 'overflow' if full?
+
     @top += 1
     @stack[@top] = x
   end
@@ -23,7 +29,36 @@ class MyStack
   # 計算量はO(1)
   def pop
     raise 'underflow' if empty?
+
     @top -= 1
     @stack[@top + 1]
   end
 end
+
+my_stack = MyStack.new
+input = gets.chomp.split
+
+input.each do |s|
+  case s
+  when '+'
+    a = my_stack.pop
+    b = my_stack.pop
+    my_stack.push(a + b)
+  when '-'
+    b = my_stack.pop
+    a = my_stack.pop
+    my_stack.push(a - b)
+  when '*'
+    a = my_stack.pop
+    b = my_stack.pop
+    my_stack.push(a * b)
+  when '/'
+    a = my_stack.pop
+    b = my_stack.pop
+    my_stack.push(a / b)
+  else
+    my_stack.push(s.to_i)
+  end
+end
+
+puts my_stack.pop
